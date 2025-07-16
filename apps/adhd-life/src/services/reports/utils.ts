46e730f97c2bc57320857483e46a31711aeb0452
evan-moon <bboydart91@gmail.com>
2025-07-17 06:17:52 +0900

    refactor: 루틴 성공율 함수 별도 분리

--- apps/adhd-life/src/services/reports/utils.ts
+++ apps/adhd-life/src/services/reports/utils.ts
@@ -1,18 +1,19 @@
 import { createNotionClient } from '@clarity-suite/notion';
+import { BlockObjectResponse } from '@notionhq/client';
 
-export function getStartAndEndOfThisWeek() {
+function getStartOfThisWeek() {
 	const now = new Date();
-	const day = now.getDay(); // 0(일)~6(토)
-	const diffToMonday = (day + 6) % 7; // 월요일이 0
+	const day = now.getDay();
+	const diffToMonday = (day + 6) % 7;
 
 	const start = new Date(now);
 	start.setDate(now.getDate() - diffToMonday);
 	start.setHours(0, 0, 0, 0);
 
-	return { start, end: now };
+	return { start, now };
 }
 
-export function getRoutineSuccessRate(
+function getRoutineSuccessRate(
 	notion: ReturnType<typeof createNotionClient>,
 	taskSnapshotDbId: string,
 	start: Date,
@@ -44,4 +45,53 @@ export function getRoutineSuccessRate(
 	).length;
 	const successRate = total === 0 ? 0 : Math.round((completed / total) * 100);
 	return { total, completed, successRate };
+}
+
+export function updateRoutineSuccessRate(
+	notion: ReturnType<typeof createNotionClient>,
+	taskSnapshotDbId: string,
+	blocks: BlockObjectResponse[],
+) {
+	const result = notion.findBlockRecursively(
+		blocks,
+		(block) =>
+			block.type === 'heading_3' &&
+			block.heading_3?.rich_text?.some((t) =>
+				t.plain_text?.includes('루틴 성공율'),
+			),
+	);
+
+	if (!result) {
+		throw new Error('"루틴 성공율" 블록을 찾을 수 없습니다.');
+	}
+
+	const { parentBlocks, index } = result;
+	const quoteBlock = parentBlocks[index + 1];
+	if (!quoteBlock || quoteBlock.type !== 'quote') {
+		throw new Error('"루틴 성공율" 다음에 Quote 블록이 없습니다.');
+	}
+
+	const { start, now } = getStartOfThisWeek();
+	const { total, completed, successRate } = getRoutineSuccessRate(
+		notion,
+		taskSnapshotDbId,
+		start,
+		now,
+	);
+
+	Logger.log(
+		`${start.toISOString()}부터 ${now.toISOString()} 사이에 시도한 총 ${total}개의 루틴 중 ${completed}개 성공`,
+	);
+
+	// 5. Quote 블록에 성공률 입력
+	notion.updateBlock(quoteBlock.id, {
+		quote: {
+			rich_text: [
+				{
+					type: 'text',
+					text: { content: `${successRate}%` },
+				},
+			],
+		},
+	});
 }
