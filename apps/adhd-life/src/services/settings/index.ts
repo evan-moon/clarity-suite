f1937bf341ba24d799f74735c1c7fcad7d68befd
evan-moon <bboydart91@gmail.com>
2025-07-20 12:03:35 +0900

    fix: adhd 빌드 에러 수정

--- apps/adhd-life/src/services/settings/index.ts
+++ apps/adhd-life/src/services/settings/index.ts
@@ -1,12 +1,9 @@
-import { applySettingsFromSheet, SettingConfig } from '@clarity-suite/settings';
+import { applySettingsFromNotionVariables } from '@clarity-suite/settings';
 import { SCRIPT_PROPERTIES_MAP, DATABASE_PROPERTIES } from './constants';
 
-const config: SettingConfig = {
-	SHEET_NAME: 'Setting',
-	SCRIPT_PROPERTIES_MAP,
-	DATABASE_PROPERTIES,
-};
-
 export function applySettingsFromSheetForAdhdLife() {
-	applySettingsFromSheet(config);
+	applySettingsFromNotionVariables({
+		SCRIPT_PROPERTIES_MAP,
+		DATABASE_PROPERTIES,
+	});
 }
--- apps/clarity-life/src/services/_shared/i18n/index.ts
+++
@@ -1,6 +0,0 @@
-import { enConfig } from './texts';
-import { I18nConfig } from './types';
-
-export function t(key: keyof I18nConfig['properties']): string {
-	return enConfig.properties[key];
-}
